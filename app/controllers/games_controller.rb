415b6dc5333a848e041f351cea5f794a1664e47b
Joshua Rieland <greengiantcoding@icloud.com>
2015-05-04 21:40:32 -0700

    finished add game for sale/trade, user dashboard received current # of games in trading post

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -7,6 +7,24 @@ class GamesController < ApplicationController
   end
 
   def create
+    if params[:trade_status] == 'Yes' and params[:trade_requirements] == [""] then flash[:trade_error] = 'Must Select A Desired Trade Title/Category' end
+    if params[:sell_status] == 'Yes' and params[:sell_price] == "" then flash[:sell_error] = 'Must Select A Sell Price' end
+    if flash[:trade_error] or flash[:sell_error] then redirect_to '/games/new' 
+    else
+      @trade_price = params[:trade_requirements].join(',') 
+      Game.create( title: params[:title], 
+                      genre: params[:genre], 
+                      description: params[:description], 
+                      quality: params[:quality], 
+                      trade_status: params[:trade_status],
+                      trade_price: @trade_price,
+                      sell_status: params[:sell_status],
+                      sell_price: params[:sell_price],
+                      user_id: session[:user_id],
+                      platform: params[:console]
+                      ) 
+      redirect_to '/dashboards/new'
+    end
   end
 
   def edit
@@ -20,4 +38,6 @@ class GamesController < ApplicationController
 
   def delete
   end
+
+ 
 end
